74115963932a4538cc71bd39df7190dfe46ca1dc
Brendon Hampton <brendonhampton1988@gmail.com>
2020-03-26 11:31:08 -0700

    added the dropdown menu with only the keywords so they wont double populate

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 'use strict';
+
+//  get data using Ajax
 $(document).ready(function(){
   $.ajax({
     url: '/data/page-01.json',
@@ -11,14 +13,20 @@ $(document).ready(function(){
 
         $('p#picList').append(`<img src='${newPics.image}'>`);
         $('p#picList').append(`<li>${newPics.title}</li>`);
-        // <li>${newPics.title}</li>
+        // call functions
+        createkeywords();
+        createDropDown();
       });
     },
   });
 });
+// empty arrays for storing info from objects
+let keywordArr = [];
 let animalArr = [];
 // console.log(animalArr);
 // console.log('objects:', animalArr);
+
+// create constructor function that takes obj
 function constructor(animal){
   this.image = animal.image_url;
   this.title = animal.title;
@@ -28,9 +36,30 @@ function constructor(animal){
   animalArr.push(this);
 }
 
+// create keywords function
 
-// $.getJSON('/data/page-01.json', function(data){
-//   console.log(data);
-// });
+function createkeywords() {
+  animalArr.forEach(animal => {
+    // if animal does not include animal keyword then push into keywords so no duplicates are made
+    if(!keywordArr.includes(animal.keyword)){
+      keywordArr.push(animal.keyword);
+    }
+  });
+}
 
-// ajax method
+// dropdown func
+function createDropDown() {
+  let select = $('#sort');
+  select.empty();
+  // append the keywords to the dropdown option tag
+  keywordArr.forEach(keyword => {
+    let sort = $(`<option value=${keyword}>${keyword}</option>`);
+    select.append(sort); 
+  });
+}
+
+
+
+// console tests
+console.log(keywordArr);
+console.log(animalArr);
