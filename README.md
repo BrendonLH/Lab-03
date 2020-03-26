ade3d8d7db242995f4d84b28f874142717a84219
Brendon Hampton <brendonhampton1988@gmail.com>
2020-03-25 23:27:22 -0700

    got images to render to the page and added some css

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ Lab-02 jquery selecotrs and events
 1. As a user, I want to view the images on the page so that I can browse the photo collection.
 2. As a user, I want to be able to filter the images so that I can view only images that match a keyword.
 3. As a user, I want a simple, clean looking UI so that my photo gallery clearly displays the images in a grid like pattern.
+
+
+## 3/25/2020 
+
+ 1. finished getting the images to display, also added the title to render underneath each picture. 
--- js/app.js
+++ js/app.js
@@ -1,19 +1,36 @@
 'use strict';
+$(document).ready(function(){
+  $.ajax({
+    url: '/data/page-01.json',
+    dataType: 'json',
+    type: 'get',
+    cache: false,
+    success: function(data) {
+      data.forEach(picture => {
+        let newPics = new constructor(picture);
+
+        $('p#picList').append(`<img src='${newPics.image}'>`);
+        $('p#picList').append(`<li>${newPics.title}</li>`);
+        // <li>${newPics.title}</li>
+      });
+    },
+  });
+});
+let animalArr = [];
+// console.log(animalArr);
+// console.log('objects:', animalArr);
+function constructor(animal){
+  this.image = animal.image_url;
+  this.title = animal.title;
+  this.description = animal.description;
+  this.keyword = animal.keyword;
+  this.horns = animal.horns;
+  animalArr.push(this);
+}
+
 
 // $.getJSON('/data/page-01.json', function(data){
 //   console.log(data);
 // });
 
 // ajax method
-$.ajax({
-  url: '/data/page-01.json',
-  dataType: 'json',
-  type: 'get',
-  cache: false,
-  success: function(data) {
-    $(data).each(function(index, value){
-      console.log(value.keyword);
-    });
-    console.log(data);
-  },
-});
